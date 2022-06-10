8e1502c46732a26704db4454bea684a39babfa9b
TANIMUL ISLAM <35636662+Tanimul@users.noreply.github.com>
2022-06-11 01:03:37 +0600

    Input Design Done

--- app/src/main/java/com/example/ethereum_blockchain/InputActivity.kt
+++ app/src/main/java/com/example/ethereum_blockchain/InputActivity.kt
@@ -3,9 +3,9 @@ package com.example.ethereum_blockchain
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class InputActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_input)
     }
 }
