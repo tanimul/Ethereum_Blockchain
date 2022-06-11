a289781e673ba000e280a7be5033aef1eb891922
TANIMUL ISLAM <35636662+Tanimul@users.noreply.github.com>
2022-06-12 00:53:29 +0600

    Layout Transaction added.

--- app/src/main/java/com/example/ethereum_blockchain/ui/InfoActivity.kt
+++ app/src/main/java/com/example/ethereum_blockchain/ui/InfoActivity.kt
@@ -3,10 +3,13 @@ package com.example.ethereum_blockchain.ui
 import android.os.Bundle
 import android.util.Log
 import androidx.lifecycle.ViewModelProvider
+import com.example.ethereum_blockchain.R
+import com.example.ethereum_blockchain.data.model.response.Response_Model
 import com.example.ethereum_blockchain.databinding.ActivityInfoBinding
 import com.example.ethereum_blockchain.utils.extentions.isNetworkAvailable
 import com.example.ethereum_blockchain.utils.extentions.toast
 import com.example.ethereum_blockchain.viewmodel.InfoViewModel
+import java.math.BigDecimal
 
 class InfoActivity : AppBaseActivity() {
     companion object {
@@ -29,18 +32,26 @@ class InfoActivity : AppBaseActivity() {
             address = intent.getStringExtra("address").toString()
         }
 
-        if(isNetworkAvailable()){
+        if (isNetworkAvailable()) {
             showProgress(true)
             infoViewModel.getInfos(address)
-
             infoViewModel.infos.observe(this) {
                 Log.d(TAG, "Data Observed: $it")
                 showProgress(false)
+                putValue(it)
             }
-        }else{
+        } else {
             showProgress(false)
             toast("Please Connect Your Internet")
         }
 
+    }
+
+    private fun putValue(it: Response_Model) {
+        binding.tvTotalBalance.text = BigDecimal(it.balance).toPlainString()
+        binding.tvTotalSendBalance.text = BigDecimal(it.total_sent).toPlainString()
+        binding.tvTotalReceivedBalance.text = BigDecimal(it.total_received).toPlainString()
+        binding.tvTotalNonce.text =
+            resources.getString(R.string.total_nonce) + " " + it.nonce.toString()
     }
 }
